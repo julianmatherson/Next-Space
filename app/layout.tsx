379759c893b3eb0f313a6b7da34624fa5939151a
julian matherson <86966959+julianmatherson@users.noreply.github.com>
2023-07-13 11:18:31 -0500

    Build an App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import NavMenu from './NavMenu';
+import './globals.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <NavMenu/>
+        {children}
+      </body>
     </html>
   )
 }
